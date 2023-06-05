ad6b11669e884ec87fd181f20812e210762e898a
Ryan <ryanmiller732@gmail.com>
2023-06-05 13:07:19 -0400

    wip: video styles

--- components/Video/index.tsx
+++ components/Video/index.tsx
@@ -1,24 +1,37 @@
 "use client"
 
-import React, { useRef, useState } from "react"
+import React, { useRef } from "react"
 import ReactPlayer, { type ReactPlayerProps } from "react-player"
 import classes from './index.module.scss';
+import { useField } from 'payload/components/forms'
 
-const Video: React.FC = () => {
+type Props = {
+    label: string
+}
 
-    const [url, setUrl] = useState("")
+const Video: React.FC = ({label}: Props) => {
+
+    const { value, setValue } = useField<string>({ path: 'videoUrl' })
     const inputRef = useRef<HTMLInputElement>(null)
 
     return (
         <>
+            <label className="field-label">{label}</label>
             <div className={classes.inputRow}>
-                <input type="url" ref={inputRef} />
-                <button onClick={() => {
+                <div className={`field-type text ${classes.textWrapper}`}>
+                    <input type="url" ref={inputRef} />
+                </div>
+                <button 
+                className={`btn btn--style-primary btn--size-medium`}
+                style={{ marginTop: 0 }}
+                type="button"
+                onClick={() => {
                     const value = inputRef?.current?.value
-                    if (value) setUrl(value)
-                }}>Set URL</button>
+                    if (value) setValue(value)
+                }}><span className="btn__content"><span className="btn__label">Set URL</span></span></button>
             </div>
-            {url && <div className={classes.playerWrapper}><ReactPlayer url={url} width="100%" height="100%" controls={true} className={classes.reactPlayer} /></div>}
+            {/**Boolean(value) && <div className={classes.playerWrapper}><ReactPlayer url={value} width="100%" height="100%" className={classes.reactPlayer} /></div>**/}
+            {/**value && <div className={classes.playerWrapper}><iframe src={value} width="100%" height="100%" className={classes.reactPlayer} /></div> **/}
         </>
     )
 }
